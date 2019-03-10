9b7478223dc29a49c7e44e1c5a580a37c662f0b4
Michael Escue <mescue@pdx.edu>
2019-03-10 01:10:44 -0800

    Setup program description

---
+++ hw5.c
@@ -0,0 +1,19 @@
+/**
+ * Assignment 5
+ * ECE 362
+ * Michael Escue
+ * 
+ * This program:
+ *  1.  Prints out the thread Id of 4 seperate threads.
+ *  2.  Breaks an array into 4 partitions.
+ *  3.  Uses one thread to sort the partition using "Bubble Sort".
+ *  4.  Merges 4 threads to 2 threads of the sorted partitions.
+ *  5.  Merges the 2 remaining partitions into one sorted array.
+ *  6.  Checks the result to ensure it is sorted.
+ */
+
+
+#define ARRAY_SIZE 64000
+
+
+int main()
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void){
-    char a[] = "Hello Lara.";
-    printf("%s", a);
-    getchar();
-    return 1;
-}
