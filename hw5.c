7326f32f6dca71bf452e2310e097bf1f250bd968
Michael Escue <mescue@pdx.edu>
2019-03-10 23:54:05 -0700

    Cleaning up unecessary files

--- hw5.c
+++ hw5.c
@@ -12,8 +12,14 @@
  *  6.  Checks the result to ensure it is sorted.
  */
 
+#include <pthread.h> // C:\TDM-GCC-64\x86_64-w64-mingw32\include\pthread.h
 
 #define ARRAY_SIZE 64000
 
+// Compare threads. Returns nonzero if equal,  otherwise.
 
-int main()
+
+int main(int argc, char * argv){
+
+    return 1;
+}
