09c59bf7d190a3c2b618f2aada6bfb9b716a73cb
Michael Escue <mescue@pdx.edu>
2019-03-11 01:40:21 -0700

    Touched-up printids.c

--- printids.c
+++ printids.c
@@ -1,7 +1,7 @@
 /**
  * @file printids.c
  * @W. Richard Stevens, Stephen A. Rago
- * @brief From the Text, "Advanced Programming in the UNIX Environment": A Thread ID printing program.
+ * @brief From the Text, "Advanced Programming in the UNIX Environment", Figure 11.2:A Thread ID printing program.
  * @ Argument *s of the function is a string prefacing the printed thread ID's.
  * 
  */
